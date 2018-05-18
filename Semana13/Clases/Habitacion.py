d45155c4119491de9dfdf59188313d7a471cbcbb
Wida7 <38042445+Wida7@users.noreply.github.com>
2018-05-18 12:58:20 -0500

    Update Habitacion.py

--- Semana13/Clases/Habitacion.py
+++ Semana13/Clases/Habitacion.py
@@ -1,15 +1,28 @@
 from Semana13.Clases.Reserva import Reserva
+""" Importamos nuestra clase de reserva"""
 
 class Habitacion:
+    """ Representa la clase de una habitacion, y su relacion con las reservas"""
 
     reservas = []
 
     def __init__(self, numero, tipo):
+        """
+        Crea una nueva instancia de Habitaciones
+        
+        :param numero: El numero de la habitacion
+        :param tipo: La caracteristica de la habitacion   
+        """
         self.numero = numero
         self.tipo = tipo
 
 
     def reservar(self, nueva_reserva):
+       """
+        Crea una nueva instancia para reserva, y valida las reservas de la habitacion
+        
+        :param nueva_reserva: Recibe una reserva y valida si esta es viable
+        """        
         for reserva in self.reservas:
             if(reserva.coliciona(nueva_reserva)):
                 raise ValueError('La habitación no se'
@@ -19,6 +32,8 @@ class Habitacion:
 
 
     def __repr__(self):
+        """ Representamos nuestra clase """
+        
         return 'Habitación # '+self.numero\
                + ' de tipo '+self.tipo+' Tiene '\
                + str(len(self.reservas))+ ' reservas'
