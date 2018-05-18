da932ff59cb3b06b7f3ef769ea531923345d80ee
Wida7 <38042445+Wida7@users.noreply.github.com>
2018-05-18 14:34:27 -0500

    Update principal.py

--- Semana13/principal.py
+++ Semana13/principal.py
@@ -3,8 +3,11 @@ from Semana13.Clases.Habitacion import Habitacion
 from Semana13.Clases.Reserva import Reserva
 from Semana13.create import crear_reservas
 from Semana13.read import ver_reservas
+"""importamos nuestras clases para relacionarlas"""
 from datetime import *
+"""importamos para utilizar formatos de date"""
 from Semana13.db_handler import *
+"""importamos la base de datos"""
 
 # cargar los hoteles
 hoteles = cargar_hoteles('bd/Hoteles.txt')
@@ -12,6 +15,8 @@ hoteles = cargar_hoteles('bd/Hoteles.txt')
 # Cargar las habitaciones
 # Cargar las reservas
 for hotel in hoteles:
+    """indicamos formato para hoteles"""
+    
     hotel.habitaciones = cargar_habitaciones(
         'bd/habitaciones/'+hotel.nombre+'.txt')
     for habitacion in hotel.habitaciones:
@@ -21,9 +26,11 @@ for hotel in hoteles:
         try:
             habitacion.reservas = cargar_reservas(archivo)
         except FileNotFoundError:
+            """Si no encuentra un archivo, crea un archivo e intenta el proceso de nuevo"""
             f = open(archivo, "w+")
 
 while True:
+    """Lanza un menu para crear o consultar reservas, (int --> str)"""
     print('Bienvenido a el vago')
     print('Seleccione una opción')
     print('1. Crear reserva')
