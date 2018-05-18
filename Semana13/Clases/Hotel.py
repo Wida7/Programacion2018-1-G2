64bd84b3ade0d1a351a4237758ebf4f188e3efe0
Wida7 <38042445+Wida7@users.noreply.github.com>
2018-05-18 13:02:35 -0500

    Update Hotel.py

--- Semana13/Clases/Hotel.py
+++ Semana13/Clases/Hotel.py
@@ -1,10 +1,18 @@
 from Semana13.Clases.Habitacion import Habitacion
+"""Importamos nuestra clase de habitacion"""
 
 class Hotel:
+    """Representa nuestro hotel"""
 
     habitaciones = []
 
     def __init__(self, nombre, estrellas, servicios):
+        """
+        Crea una nueva instancia de hotel
+        :param nombre: nombre del hotel
+        :param estrellas: estrellas del hotel
+        :param servicios: prestacion de servicios del hotel
+        """
         self.nombre = nombre
         self.estrellas = estrellas
         self.servicios = servicios
