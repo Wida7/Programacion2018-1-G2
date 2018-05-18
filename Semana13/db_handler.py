34916c6fe8594c6b94c71ab4c01dc635fe5dcef8
Wida7 <38042445+Wida7@users.noreply.github.com>
2018-05-18 14:23:59 -0500

    Update db_handler.py

--- Semana13/db_handler.py
+++ Semana13/db_handler.py
@@ -1,18 +1,25 @@
 from Semana13.Clases.Hotel import Hotel
 from Semana13.Clases.Habitacion import Habitacion
 from Semana13.Clases.Reserva import Reserva
+"""importamos nuestras clases para las relaciones"""
 from datetime import *
+"""importamos para utilizar formatos de date"""
 
 def cargar_hoteles(file):
     """
-
-    :param file:
-    :return:
+    Creamos una base de datos en un ducumento txt conforme al objeto de hoteles
+    
+    :param file: hoteles
+    :return: abrir base de datos(txt) de hoteles
     """
     hoteles_resultante = []
     with open(file) as bd_Hoteles:
+        """abrimos nuestro archivo para agregar los datos de hoteles"""
+        
         lineas = bd_Hoteles.readlines()
         for linea in lineas:
+            """damos formato a la base de datos, con posiciones y separadores"""
+            
             variables = linea.strip().split(',')
             hoteles_resultante.append(Hotel(variables[0],
                                  variables[1],
@@ -22,10 +29,20 @@ def cargar_hoteles(file):
 
 
 def cargar_habitaciones(file):
+    """
+    Creamos una base de datos en un ducumento txt conforme al objeto de habitaciones
+    
+    :param file: habitaciones
+    :return: abrir base de datos(txt) de habitaciones
+    """
     habitaciones_resultantes = []
     with open(file) as bd_Habitcion:
+        """abrimos nuestro archivo para agregar los datos de habitaciones"""
+        
         lineas = bd_Habitcion.readlines()
         for linea in lineas:
+            """damos formato a la base de datos"""
+            
             variables = linea.strip().split(',')
             habitaciones_resultantes.append(Habitacion(variables[0],
                                                        variables[1]))
@@ -33,10 +50,22 @@ def cargar_habitaciones(file):
     return habitaciones_resultantes
 
 def cargar_reservas(file):
+    """
+    Creamos una base de datos en un ducumento txt conforme al objeto de reservas
+    
+    :param file: reservas
+    :return: abrir base de datos(txt) de habitaciones
+    """
+    
     reservas_resultantes = []
     with open(file) as bd_reservas:
+        """abrimos nuestro documentos txt para guardar los datos"""
         lineas = bd_reservas.readlines()
+        """relacionamos nuestros datos de (bd_reservas) para guardarlos"""
+        
         for linea in lineas:
+            """damos formato a la base de datos"""
+            
             variables = linea.strip().split(',')
             fecha_inicio = datetime.strptime(
                 variables[2],'%Y-%m-%d').date()
@@ -53,6 +82,13 @@ def cargar_reservas(file):
 
 
 def guardar_reserva(hotel, habitacion, reserva):
+    """
+    Funcion para guardar la reserva en la base de datos
+    :param hotel: nombre hotel
+    :param habitacion: nombre habitacion
+    :param reserva: datos de reserva
+    :return: guardar la reserva en una base de datos
+    """
     f = open('bd/reservas/' +
              hotel.nombre +
              habitacion.numero +
